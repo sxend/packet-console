5a5187124a69690070e21553769362363460d3b8
sxend <arimitsu.k+github@gmail.com>
2015-04-17 19:40:50 +0900

    add test

---
+++ src/test/scala/arimitsu/sf/packetconsole/RouterSpec.scala
@@ -0,0 +1,10 @@
+package arimitsu.sf.packetconsole
+
+import org.scalatest._
+
+class RouterSpec extends FlatSpec with Matchers {
+  "Endpoint" should "return statics" in {
+    val method = "GET"
+    val path = "/statistics"
+  }
+}
