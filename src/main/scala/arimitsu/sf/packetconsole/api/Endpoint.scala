3c70cdddf3c385467b37f7346e7eab5cdb1ec15c
sxend <arimitsu.k+github@gmail.com>
2015-04-17 21:49:31 +0900

    add endpoint configuration

--- src/main/scala/arimitsu/sf/packetconsole/api/Endpoint.scala
+++ src/main/scala/arimitsu/sf/packetconsole/api/Endpoint.scala
@@ -1,8 +1,10 @@
 package arimitsu.sf.packetconsole.api
 
 import akka.actor.ActorSystem
+import akka.event.LoggingAdapter
 import akka.http._
-import akka.http.server.Route
+import akka.http.model.HttpRequest
+import akka.http.server._
 import akka.stream.ActorFlowMaterializer
 import akka.stream.scaladsl.Sink
 
@@ -10,11 +12,22 @@ class Endpoint(components: {
   val system: ActorSystem
   val route: Route
 }) {
-  def start(): Unit = {
+  implicit val system = components.system
 
-    implicit val materializer = ActorFlowMaterializer()
+  import system.dispatcher
+
+  def start(): Unit = {
     val host = System.getProperty("packet.console.host", "0.0.0.0")
     val port = System.getProperty("packet.console.port", "51600").toInt
+
+    implicit val routingLog = new RoutingLog() {
+      override def log: LoggingAdapter = system.log
+
+      override def requestLog(request: HttpRequest): LoggingAdapter = log
+    }
+    implicit val materializer = ActorFlowMaterializer()
+    implicit val settings = RoutingSettings.default(system)
+    implicit val setup = RoutingSetup.apply
     val server = Http(components.system).bind(interface = host, port = port)
     server.to {
       Sink.foreach { connection =>
